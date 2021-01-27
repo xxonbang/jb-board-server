06bdfba08c98001c2f7d03b9b091c805f0ca64c6
xxonbang <mackulri@gmail.com>
2021-01-27 13:26:05 +0900

    - cors 라이브러리 추가

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import helmet from "helmet";  // http header 설정을 바꿔줌 (보안 목적)
 import morgan from "morgan";  // server logger module (3rd party log 모듈)
 import bodyParser from "body-parser";  // client 의 post, put request 의 body 내용을 parsing 후 req.body 에 담음
+import cors from 'cors';
 
 import routes from "./routes.js";
 import globalRouter from "./routers/globalRouter.js";
@@ -10,6 +11,8 @@ import userRouter from "./routers/userRouter.js";
 import boardRouter from "./routers/boardRouter.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use(helmet());
 app.use(morgan('dev'));
